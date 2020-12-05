75966de7d540675f015b10de1d50b69138d0d3bc
nyahehnagi <bromley.jones@gmail.com>
2020-12-05 11:48:00 +0000

    day 5

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -6,14 +6,16 @@ fun day5(){
     println(calculateMySeat(calculateIDs(boardingPasses)))
 }
 
+// take the last ID in the list as sorted ascending
 fun calculateHighestID(IDs : List<Int>) = IDs.last()
 
+// Pair up the seats and compare to find missing seat
 fun calculateMySeat(IDs : List<Int>) =
         IDs.zipWithNext()
             .first { (first, second) -> second != first + 1 }.first + 1
 
 
-
+//turn each row into a binary number and convert to Integer
 fun calculateIDs (boardingPasses : List<String>) =
         boardingPasses.map { it.replace(Regex("[RB]"), "1").replace(Regex("[FL]"), "0") }
         .map { it.toInt(2) }
